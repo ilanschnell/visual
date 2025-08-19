f013a4c533d293eb9ec0756921c00d58730708c8
Ilan Schnell <ilanschnell@gmail.com>
2025-08-18 20:33:41 -0500

    simplify

--- dubner/draw.py
+++ dubner/draw.py
@@ -3,11 +3,8 @@ from PIL import Image, ImageDraw, ImageFont
 from bitarray.util import zeros, gen_primes
 
 
-def draw_bitarray(a, size, colors=None, font=None, imap=lambda i: i):
-    if colors is None:
-        colors = ['#eee', '#369', '#000', '#fff']
-    if font is None:
-        font = ImageFont.truetype("Vera.ttf", 0.36 * size)
+def draw_bitarray(a, size, colors):
+    font = ImageFont.truetype("Vera.ttf", 0.36 * size)
 
     im = Image.new('RGB', (size * len(a) + 1, size + 1))
     dr = ImageDraw.Draw(im)
@@ -15,10 +12,9 @@ def draw_bitarray(a, size, colors=None, font=None, imap=lambda i: i):
         dr.rectangle([(i * size, 0), ((i+1) * size, size)],
                      fill=colors[v],
                      outline="black")
-        if font:
-            dr.text(((i + 0.15) * size, 0.3 * size),
-                    str(imap(i)),
-                    fill=colors[v + 2], font=font)
+        dr.text(((i + 0.15) * size, 0.3 * size),
+                str(6 * i),
+                fill=colors[v + 2], font=font)
     return im
 
 def dubner():
@@ -32,20 +28,20 @@ def dubner():
 
     sqr = 50    # square size
     space = 20  # spacing
-    imap = lambda i: 6 * i
     size = (2 * space + 25 * sqr,
             4 * space + 12 * sqr)
     print(size)
     im = Image.new('RGB', size, color='#fff')
     colors = ['#eee', '#fa7', '#000', '#000']
-    im.paste(draw_bitarray(middles[:25], sqr, colors, imap=imap),
+    im.paste(draw_bitarray(middles[:25], sqr, colors),
              (space, space))
 
+    colors = ['#eee', '#369', '#000', '#fff']
     for i, j in enumerate(middles.search(1, 0, 24)):
-        im.paste(draw_bitarray(middles[:25 - j], sqr, None, imap=imap),
+        im.paste(draw_bitarray(middles[:25 - j], sqr, colors),
                  (space + j * sqr, 2 * space + sqr + i * sqr))
 
-    im.paste(draw_bitarray(mark[:25], sqr, None, imap=imap),
+    im.paste(draw_bitarray(mark[:25], sqr, colors),
              (space, 3 * space + 11 * sqr))
 
     im.save('image.png')
